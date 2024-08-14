e10c29dc0cebf2323fdaf1a56fc40e7ceef326d5
Miraç KELEŞ <64755870+Miraclkeles@users.noreply.github.com>
2024-08-14 04:12:43 +0200

    optimize the naming

--- skill_model.ts
+++ skill_model.ts
@@ -1,15 +1,6 @@
-const userIds = [/* array of ObjectId's */];
-const skills = await Promise.all(
-  userIds.map(userId => SkillModel.find({ linkedUserId: userId }))
-);
-
-const userIds = [/* array of ObjectId's */];
 const skills = await SkillModel.find({ linkedUserId: { $in: userIds } });
-
-const skills = await SkillModel.find().lean();
-
+const allSkills = await SkillModel.find().lean();
 const skillsList = await SkillModel.find({}, 'title price').lean();
-
 const aggregatedSkills = await SkillModel.aggregate([
   {
     $group: {
