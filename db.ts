acbec6eae57db8dc6c2b18601bad29102dbc0494
Miraç KELEŞ <64755870+Miraclkeles@users.noreply.github.com>
2024-08-23 14:14:23 +0200

    add readability

--- db.ts
+++ db.ts
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const mongoDBConnectionURI: string = process.env.MONGODB_URI || '';
 
-mongoose.connect(mongoDBConnectionURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Successfully connected to MongoDB.'))
-.catch((error) => console.error('Connection error with MongoDB:', error));
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(mongoDBConnectionURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Successfully connected to MongoDB.');
+  } catch (error) {
+    console.error('Connection error with MongoDB:', error);
+  }
+};
+
+connectToMongoDB();
 
 export default mongoose.connection;
