e80cbbffa9a1c25401092666400ba939886f4155
Miraç KELEŞ <64755870+Miraclkeles@users.noreply.github.com>
2024-08-24 19:46:03 +0200

    implemented feature

--- db.ts
+++ db.ts
@@ -1,19 +1,54 @@
-import mongoose from 'mongoose';
+interface MongoDBEventHandlers {
+  onConnected?: () => void;
+  onError?: (error: any) => void;
+  onDisconnected?: () => void;
+}
+
+import mongoose, { ConnectionOptions } from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const mongoDBConnectionURI: string = process.env.MONGODB_URI || '';
 
-const connectToMongoDB = async () => {
+const connectionOptions: ConnectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+};
+
+const defaultEventHandlers: MongoDBEventHandlers = {
+  onConnected: () => console.log('Successfully connected to MongoDB.'),
+  onError: (error) => console.error('MongoDB connection error:', error),
+  onDisconnected: () => console.log('MongoDB connection disconnected.'),
+};
+
+const connectToMongoDB = async (eventHandlers: MongoDBEventHandlers = defaultEventHandlers) => {
   try {
-    await mongoose.connect(mongoDBConnectionURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    const db = mongoose.connection;
+
+    db.on('connecting', () => console.log("MongoDB: Connecting..."));
+    db.on('error', (error) => {
+      if (eventHandlers.onError) eventHandlers.onError(error);
+      mongoose.disconnect();
     });
-    console.log('Successfully connected to MongoDB.');
+    db.on('connected', () => {
+      if (eventHandlers.onConnected) eventHandlers.onConnected();
+    });
+    db.once('open', () => console.log("MongoDB: Connection is open..."));
+    db.on('reconnected', () => console.log("MongoDB: Reconnected!"));
+    db.on('disconnected', () => {
+      if (eventHandlers.onDisconnected) eventHandlers.onDisconnected();
+      console.log("MongoDB: Disconnected. Attempting to reconnect...");
+      setTimeout(() => {
+        connectToMongoDB(eventHandlers);
+      }, connectionOptions.reconnectInterval);
+    });
+
+    await mongoose.connect(mongoDBConnectionURI, connectionOptions);
   } catch (error) {
-    console.error('Connection error with MongoDB:', error);
+    if (eventHandlers.onError) eventHandlers.onError(error);
   }
 };
 
